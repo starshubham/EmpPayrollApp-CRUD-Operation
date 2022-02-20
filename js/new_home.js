e0229d35b8658b531ecef2beec1987bb43e6f4fe
Shubham Seth <shubhamsth1234@gmail.com>
2022-02-20 11:53:14 +0530

    [add] Shubham added EmpPayrollApp-CRUDOperation program

---
+++ js/new_home.js
@@ -0,0 +1,61 @@
+let empPayrollList;
+window.addEventListener('DOMContentLoaded', (event) => {
+    empPayrollList = getEmployeePayrollDataFromStorage();
+    document.querySelector(".emp-count").textContent = empPayrollList.length;
+    createInnerHTML();
+    localStorage.removeItem('editEmp');
+});
+
+const getEmployeePayrollDataFromStorage = () => {
+    return localStorage.getItem('EmployeePayrollList') ? 
+            JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];  
+}
+
+const createInnerHTML = () => {
+    const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>"+
+                     "<th>Salary</th><th>Start Date</th><th>Actions</th>";
+    if (empPayrollList.length == 0) return;
+    let innerHtml = `${headerHtml}`;
+    for (const empPayrollData of empPayrollList) {
+      innerHtml = `${innerHtml}
+      <tr>
+        <td><img class="profile" alt="" 
+                  src="${empPayrollData._profilePic}">
+        </td>
+        <td>${empPayrollData._name}</td>
+        <td>${empPayrollData._gender}</td>
+        <td>${getDeptHtml(empPayrollData._department)}</td>
+        <td>${empPayrollData._salary}</td>
+        <td>${stringifyDate(empPayrollData._startDate)}</td>
+        <td>
+          <img id="${empPayrollData._id}" onclick="remove(this)" 
+             src="../assets/icons/delete-black-18dp.svg" alt="delete">
+          <img id="${empPayrollData._id}" onclick="update(this)" 
+             src="../assets/icons/create-black-18dp.svg" alt="edit">
+        </td>
+      </tr>
+      `;
+    }
+    document.querySelector("#table-display").innerHTML = innerHtml;
+}
+
+const getDeptHtml = (deptList) => {
+  let deptHtml = '';
+  for (const dept of deptList) {
+    deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`
+  }
+  return deptHtml;
+}
+
+// Remove a particular employee from homePage and hence update the localstorage to fetch the data.
+const remove = (node) => {
+  let empPayrollData = empPayrollList.find(empData => empData._id == node.id);
+  if (!empPayrollData) return;
+  const index = empPayrollList
+                .map(empData => empData._id)
+                .indexOf(empPayrollData._id);
+  empPayrollList.splice(index, 1);
+  localStorage.setItem("EmployeePayrollList", JSON.stringify(empPayrollList));
+  document.querySelector(".emp-count").textContent = empPayrollList.length;
+  createInnerHtml();
+}
